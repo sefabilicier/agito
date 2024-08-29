0b694fa2e36094cef341d8117412fa73662a98fa
sefabilicier <sefabilicier@gmail.com>
2024-08-29 16:43:56 +0300

    ReactUIImplementation

--- agitoo/src/main/java/intern/customer/agitoo/Configuration/LoggingConfiguration.java
+++ agitoo/src/main/java/intern/customer/agitoo/Configuration/LoggingConfiguration.java
@@ -3,7 +3,6 @@ package intern.customer.agitoo.Configuration;
 import intern.customer.agitoo.AgitooApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
